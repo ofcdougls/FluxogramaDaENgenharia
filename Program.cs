85a3239bb110303456a70dfaa343718fe6b7b105
ofcdougls <ofc.dougls@outlook.com>
2023-04-30 17:36:48 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿string recado;
+Console.WriteLine("O obijeto esta em movimento(S/N)? ");
+bool EstaMovimentando = Convert.ToBoolean(Console.ReadLine()!.Substring(0,1).ToUpper() == "S");
+
+
+Console.WriteLine("Deveria?");
+bool deveria = Convert.ToBoolean(Console.ReadLine());
+if (EstaMovimentando)
+{
+    if (deveria)
+    {
+        recado = "Perfeito !";
+    }
+    else
+    {
+        recado = "Use WD-40.";
+    }
+}
+else 
+{
+    if (deveria)
+    {
+        recado = "Perfeito !";
+    }
+    else 
+    {
+        recado = "Use Silver Tape.";
+    }
+}
+Console.WriteLine(recado);
